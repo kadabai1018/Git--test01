99905eee5a4730495e6f022e6a611b896cfa643e
lijiawen <1298988832@qq.com>
2023-01-08 23:19:29 +0800

    git desk

--- test.c
+++ test.c
@@ -5,11 +5,12 @@ int main(){
 	scanf("%d",&a);
 	printf("a=%d\n",a);
 	int b = 5;
-	printf("%d\n",a+b); master1 || hot-fix2
+	printf("%d\n",a+b); //master1 || hot-fix2
 	//hot fix 修改
 	//testing fecth
 	//模拟另一个人修改 wangwu
 	//SSH秘钥拉取
 	//SSh秘钥推送
+    //git desk
 	return 0;
 }
